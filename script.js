877de8c40c7c817549c8b9fffc98158895ac63ad
Rey-A-Vasquez <official.rv.7394@gmail.com>
2026-05-10 18:44:00 -0700

    reworks

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const closeNav = document.getElementById(`closeNav`);
+const openNavs = document.querySelectorAll(`.openNav`);
+const navLinks = document.querySelectorAll(`nav a`);
 
 closeNav.addEventListener("click", ()=>{
     document.getElementById(`navOverlay`).classList.add(`notActive`);
+    document.documentElement.classList.remove(`noScroll`)
+})
+
+openNavs.forEach( nav =>{
+    nav.addEventListener(`click`, ()=>{
+        document.getElementById(`navOverlay`).classList.remove(`notActive`);
+        document.documentElement.classList.add(`noScroll`);
+    })
+})
+
+navLinks.forEach(link =>{
+    link.addEventListener(`click`, ()=>{
+        document.getElementById(`navOverlay`).classList.add(`notActive`);
+        document.documentElement.classList.remove(`noScroll`)
+    })
 })
